Fix reversing params with units and add examples

pywinds/test/test_wind_functions.py
@@ -24,7 +24,7 @@ class TestCase:
         self.center = center
         self.j_displacements, self.i_displacements = displacements(lat_0, lon_0, displacement_data=displacement_data,
                                                                    shape=shape)[:, j, i]
-        area_definition = area(lat_0, lon_0, displacement_data=displacement_data, shape=shape, i=i, j=j)
+        area_definition = area(lat_0, lon_0, displacement_data=displacement_data, shape=shape)
         self.shape = (area_definition.height, area_definition.width)
         # Output data
         self.speed = speed
@@ -50,11 +50,11 @@ class TestPywinds(unittest.TestCase):
                                         v=1312.73783, old_lat=18.825, old_long=-142.64162, new_lat=89.58692,
                                         new_long=-45.03963, old_x=-8135000.0, old_y=11494327.91718, new_x=-35000.0,
                                         new_y=3394327.91718))
-        displacement_data = np.array(([x for x in range(100)], [x for x in range(100)])) * 10
+        displacement_data = (np.array([x for x in range(100)]) * 10, np.array([x for x in range(100)]) * 20)
         self.test_cases.append(TestCase(displacement_data, pixel_size=5, lat_0=90, lon_0=20, i=1, j=8,
-                                        units='km', center=(40, 10), speed=834.60569,
-                                        angle=95.29823, u=831.03988, v=-77.06734, old_lat=44.01303, old_long=-50.25507,
-                                        new_lat=39.84993, new_long=9.86386, old_x=-5101407.88566, old_y=-1831082.99511,
+                                        units='km', center=(40, 10), speed=1190.02614,
+                                        angle=69.74556, u=1116.44031, v=411.97482, old_lat=17.54702, old_long=-58.68524,
+                                        new_lat=39.84993, new_long=9.86386, old_x=-9151407.88566, old_y=-1831082.99511,
                                         new_x=-1051407.88566, new_y=-5881082.99511))
 
     def test_wind_info(self):
@@ -116,13 +116,13 @@ class TestPywinds(unittest.TestCase):
         for case in self.test_cases:
             old_lat_ji, old_long_ji = lat_long(case.lat_0, case.lon_0, projection=case.projection, i=case.i,
                                                displacement_data=case.displacement_data,
-                                                       j=case.j, shape=case.shape, pixel_size=case.pixel_size,
+                                                       j=case.j, pixel_size=case.pixel_size,
                                                        center=case.center, units=case.units, image_geod=case.image_geod,)
             new_lat_ji, new_long_ji = lat_long(case.lat_0, case.lon_0,
                                                        projection=case.projection, i=case.i, j=case.j, shape=case.shape,
                                                        pixel_size=case.pixel_size, center=case.center, units=case.units,
                                                        image_geod=case.image_geod, no_save=True)
-            old_lat, old_long = lat_long(case.lat_0, case.lon_0, projection=case.projection, shape=case.shape,
+            old_lat, old_long = lat_long(case.lat_0, case.lon_0, projection=case.projection,
                                          displacement_data=case.displacement_data,
                                                  pixel_size=case.pixel_size, center=case.center, units=case.units,
                                                  image_geod=case.image_geod)
@@ -165,7 +165,7 @@ class TestPywinds(unittest.TestCase):
 
 
 def suite():
-    """The test suite for test_main."""
+    """The test suite for test_wind_functions."""
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(TestPywinds))

pywinds/test/test_wrappers.py
@@ -27,7 +27,7 @@ class TestCase:
         self.area_extent = area_extent
         self.j_displacements, self.i_displacements = displacements(lat_0, lon_0, displacement_data=displacement_data,
                                                                    shape=shape, i=i, j=j, no_save=True)
-        area_definition = area(lat_0, lon_0, displacement_data=displacement_data, shape=shape, i=i, j=j)
+        area_definition = area(lat_0, lon_0, displacement_data=displacement_data, shape=shape)
         self.shape = (area_definition.height, area_definition.width)
         # Output data
         self.speed = speed
@@ -45,15 +45,15 @@ class TestWrappers(unittest.TestCase):
         self.test_cases = []
         self.test_cases.append(TestCase('./test_files/test_data_three.flo',
                                         i=1, j=4, pixel_size='10:km', lat_0=60, lon_0=0, center=(90, 0),
-                                        area_extent=(-25000.0, 3404327.92, 25000.0, 3454327.92),
-                                        speed=2820.83, angle=42.94, u=1921.49, v=2065.18, old_lat=89.81,
-                                        old_long=-26.58, new_lat=-21.9, new_long=-151.31))
+                                        area_extent=(-25000.0, 3404327.91717, 25000.0, 3454327.91717),
+                                        speed=2820.83, angle=42.94, u=1921.49, v=2065.18, old_lat=-21.9,
+                                        old_long=-151.31, new_lat=89.81, new_long=-26.58))
         displacement_data = np.array(([x for x in range(25)], [x for x in range(25)])) * 10
         self.test_cases.append(TestCase(displacement_data.tolist(), pixel_size=5, lat_0=90, lon_0=20, i=1, j=4,
                                         units='km', center=(40, 10),
-                                        area_extent=(-1046407.89, -5876083, -1021407.89, -5851083),
-                                        speed=208.14, angle=118.39, u=183.1, v=-98.98, old_lat=39.92, old_long=9.97,
-                                        new_lat=45.27, new_long=-3.42))
+                                        area_extent=(-1046407.88566, -5876082.99511, -1021407.88566, -5851082.99511),
+                                        speed=208.14, angle=118.39, u=183.1, v=-98.98, old_lat=45.27, old_long=-3.42,
+                                        new_lat=39.92, new_long=9.97))
 
     def test_wind_info(self):
         for case in self.test_cases:
@@ -119,23 +119,22 @@ class TestWrappers(unittest.TestCase):
 
     def test_lat_long(self):
         for case in self.test_cases:
-            old_lat_ji, old_long_ji = args_to_data(['../lat_long.py', case.lat_0, case.lon_0,
+            old_lat_ji, old_long_ji = args_to_data(['../lat_long.py', case.lat_0, case.lon_0, '--displacement_data', case.displacement_data,
                                                     '--projection', case.projection, '--j', str(case.j), '--i',
-                                                    str(case.i), '--pixel_size', case.pixel_size, '--shape',
-                                                    str(case.shape).replace(' ', ''), '--center', case.center,
+                                                    str(case.i), '--pixel_size', case.pixel_size, '--center', case.center,
                                                     '--units', case.units, '--image_geod', case.image_geod, '--no_save'])
             new_lat_ji, new_long_ji = args_to_data(['../lat_long.py', case.lat_0, case.lon_0,
-                                                    '--displacement_data', case.displacement_data, '--projection',
-                                                    case.projection, '--j', str(case.j), '--i', str(case.i),
+                                                    '--projection', case.projection, '--shape', str(case.shape).replace(' ', ''),
+                                                    '--j', str(case.j), '--i', str(case.i),
                                                     '--pixel_size', case.pixel_size, '--center', case.center,
                                                     '--units', case.units, '--image_geod', case.image_geod, '--no_save'])
-            old_lat, old_long = args_to_data(['../lat_long.py', case.lat_0, case.lon_0,
+            old_lat, old_long = args_to_data(['../lat_long.py', case.lat_0, case.lon_0, '--displacement_data', case.displacement_data,
                                               '--projection', case.projection, '--pixel_size', case.pixel_size,
-                                              '--shape', str(case.shape).replace(' ', ''), '--center',
+                                              '--center',
                                               case.center, '--units', case.units, '--image_geod', case.image_geod, '--no_save'])
             new_lat, new_long = args_to_data(['../lat_long.py', case.lat_0, case.lon_0,
-                                              '--displacement_data', case.displacement_data, '--projection',
-                                              case.projection, '--pixel_size', case.pixel_size, '--center',
+                                              '--projection', case.projection, '--shape', str(case.shape).replace(' ', ''),
+                                              '--pixel_size', case.pixel_size, '--center',
                                               case.center, '--units', case.units, '--image_geod', case.image_geod, '--no_save'])
             self.assertEqual(case.old_lat, old_lat_ji)
             self.assertEqual(case.old_long, old_long_ji)
@@ -148,11 +147,11 @@ class TestWrappers(unittest.TestCase):
 
     def test_displacements(self):
         for case in self.test_cases:
-            displacements = args_to_data(['../displacements.py',
-                                          '--displacement_data', case.displacement_data, '--no_save'])
+            displacement = args_to_data(['../displacements.py',
+                                         '--displacement_data', case.displacement_data, '--no_save'])
             displacements_ji = args_to_data(['../displacements.py', '--displacement_data',
                                              case.displacement_data, '--j', str(case.j), '--i', str(case.i), '--no_save'])
-            j_displacements, i_displacements = displacements
+            j_displacements, i_displacements = displacement
             j_displacements_ji, i_displacements_ji = displacements_ji
             self.assertEqual(case.j_displacements, j_displacements_ji)
             self.assertEqual(case.i_displacements, i_displacements_ji)
@@ -183,7 +182,7 @@ def args_to_data(commands):
 
 
 def suite():
-    """The test suite for test_main."""
+    """The test suite for test_wrappers."""
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(TestWrappers))

pywinds/wind_functions.py
@@ -24,6 +24,7 @@ def _no_save(data, output_filename, input_filename):
         pass
     if np.size(data) == 1:
         data = np.ravel(data)
+    data = np.array(data, dtype=float)
     np.ndarray.tofile(data, os.path.join(os.getcwd(), extenion + '_output', output_filename + '.out'),
                       format='%.2f')
     np.savetxt(os.path.join(os.getcwd(), extenion + '_output', output_filename + '.txt'), data, fmt='%.2f')
@@ -54,14 +55,16 @@ def _extrapolate_j_i(j, i, shape):
 
 def _reverse_param(param):
     """Reverses the order of parameters (y/x-form is given, but most packages need x/y-form."""
-    if np.size(param) == 1:
-        return param
+    units = None
     if hasattr(param, 'units'):
-        return DataArray(list(reversed(param.data.tolist())), attrs={'units': param.units})
-    elif isinstance(param, DataArray):
-        return list(reversed(param.data.tolist()))
-    else:
-        return list(reversed(param))
+        units = param.units
+    if isinstance(param, DataArray):
+        param = param.data.tolist()
+    elif np.shape(param) != ():
+        param = list(reversed(param))
+    if units is not None:
+        return DataArray(param, attrs={'units': units})
+    return param
 
 
 def _to_int(num, error):
@@ -150,7 +153,6 @@ def _create_area(lat_0, lon_0, projection='stere', area_extent=None, shape=None,
             image_geod, type(image_geod)))
     proj_dict = proj4_str_to_dict('+lat_0={0} +lon_0={1} +proj={2} {3}'.format(lat_0, lon_0, projection,
                                                                                image_geod.initstring))
-
     area_definition = create_area_def('pywinds', proj_dict, area_extent=area_extent, shape=shape,
                            upper_left_extent=upper_left_extent, resolution=pixel_size,
                            center=center, radius=radius, units=units)
@@ -167,18 +169,20 @@ def _find_displacements(displacement_data=None, j=None, i=None, shape=None):
         j_displacement = displacement[1::2]
         i_displacement = displacement[0::2]
     elif displacement_data is not None:
-        if len(np.shape(displacement_data)) != 3 or np.shape(displacement_data)[0] != 2:
-            raise ValueError('displacement_data should have shape (2, y, x), but instead has shape {0}'.format(
+        if len(np.shape(displacement_data)) != 2 and len(np.shape(displacement_data)) != 3 or np.shape(displacement_data)[0] != 2:
+            raise ValueError('displacement_data should have shape (2, y * x) or (2, y, x), but instead has shape {0}'.format(
                 np.shape(displacement_data)))
-        j_displacement = np.array(displacement_data[1])
-        i_displacement = np.array(displacement_data[0])
+        if len(np.shape(displacement_data)) != 2:
+            displacement_data = np.reshape(displacement_data, (2, int(np.size(displacement_data) / 2)))
+        j_displacement = np.array(displacement_data[0])
+        i_displacement = np.array(displacement_data[1])
     else:
         return shape, 0, 0
     if shape is None:
-        shape = (np.size(i_displacement)**.5, np.size(j_displacement)**.5)
+        shape = [np.size(i_displacement)**.5, np.size(j_displacement)**.5]
         error = 'Shape was not provided and shape found from file was not comprised of integers: ' \
                 '{0} pixels made a shape of {1}'.format(np.size(j_displacement) + np.size(i_displacement),
-                                                        tuple([2] + list(shape)))
+                                                        tuple([2] + shape))
         shape = (_to_int(shape[0], ValueError(error)), _to_int(shape[1], ValueError(error)))
     if j is None and i is None:
         return shape, j_displacement, i_displacement
@@ -413,7 +417,7 @@ def displacements(lat_0=None, lon_0=None, displacement_data=None, projection='st
     if no_save:
         _no_save(j_displacement, 'j_displacement', displacement_data)
         _no_save(i_displacement, 'i_displacement', displacement_data)
-    return np.array((j_displacement, i_displacement))
+    return np.array((j_displacement, i_displacement), dtype=float)
 
 
 def velocity(lat_0, lon_0, delta_time, displacement_data=None, projection='stere', j=None, i=None,
@@ -484,7 +488,7 @@ def velocity(lat_0, lon_0, delta_time, displacement_data=None, projection='stere
     if no_save is False:
         _no_save(speed, 'speed', displacement_data)
         _no_save(angle, 'angle', displacement_data)
-    return np.array((speed, angle))
+    return np.array((speed, angle), dtype=float)
 
 
 def vu(lat_0, lon_0, delta_time, displacement_data=None, projection='stere', j=None, i=None,
@@ -552,7 +556,7 @@ def vu(lat_0, lon_0, delta_time, displacement_data=None, projection='stere', j=N
     if no_save is False:
         _no_save(v, 'v', displacement_data)
         _no_save(u, 'u', displacement_data)
-    return np.array((v, u))
+    return np.array((v, u), dtype=float)
 
 
 def lat_long(lat_0, lon_0, displacement_data=None, projection='stere', j=None, i=None,
@@ -634,7 +638,7 @@ def lat_long(lat_0, lon_0, displacement_data=None, projection='stere', j=None, i
             _no_save(old_long, 'old_longitude', displacement_data)
             _no_save(new_lat, 'new_latitude', displacement_data)
             _no_save(new_long, 'new_longitude', displacement_data)
-    return np.array((old_lat, old_long))
+    return np.array((old_lat, old_long), dtype=float)
 
 
 def wind_info(lat_0, lon_0, delta_time, displacement_data=None, projection='stere', j=None, i=None,