fa52b7946e2ab40dd0e63df529a47731d7f0e8b8
wroberts <wroberts4@wisc.edu>
2019-02-28 18:07:49 -0600

    Replace some 3dwinds with pywinds in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = '3dwindsdoc'
+htmlhelp_basename = 'pywindsdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -130,7 +130,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, '3dwinds.tex', '3dwinds Documentation',
+    (master_doc, 'pywinds.tex', 'pywinds Documentation',
      'Will Roberts', 'manual'),
 ]
 
@@ -140,7 +140,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, '3dwinds', '3dwinds Documentation',
+    (master_doc, 'pywinds', 'pywinds Documentation',
      [author], 1)
 ]
 
@@ -151,8 +151,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, '3dwinds', '3dwinds Documentation',
-     author, '3dwinds', 'One line description of project.',
+    (master_doc, 'pywinds', 'pywinds Documentation',
+     author, 'pywinds', 'One line description of project.',
      'Miscellaneous'),
 ]
 
