2624b904f4a602d673643c081677889f9079c864
wroberts4 <38170479+wroberts4@users.noreply.github.com>
2019-03-20 23:12:18 -0700

    Try to use negatives with command lines

--- pywinds/wrapper_utils.py
+++ pywinds/wrapper_utils.py
@@ -6,7 +6,7 @@ import sys
 import traceback
 import xarray
 from glob import glob
-from inspect import getfullargspec
+from io import StringIO
 
 import numpy as np
 
@@ -32,20 +32,33 @@ def area_to_string(area_dict):
 
 
 def _num_and_units(var):
-    print(var)
-    match = re.match("([0-9]+)([a-z]+)", var, re.I)
-    if match:
-        val, units = match.groups()
-        return _nums_or_string(val), units
-    match = re.match("([0-9]+)(\.)([a-z]+)", var, re.I)
-    if match:
-        num, period, units = match.groups()
-        return _nums_or_string(num + period), units
-    match = re.match("([0-9]+)(\.)([0-9]+)([a-z]+)", var, re.I)
-    if match:
-        num, period, decimals, units = match.groups()
-        return _nums_or_string(num + period + decimals), units
-    return _nums_or_string(var)
+   match = re.match("([0-9]+)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+   match = re.match("(-[0-9]+)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+
+   match = re.match("([0-9]+\.)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+   match = re.match("(-[0-9]+\.)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+
+   match = re.match("([0-9]+\.[0-9]+)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+   match = re.match("(-[0-9]+\.[0-9]+)([a-z]+)", var, re.I)
+   if match:
+       val, units = match.groups()
+       return _nums_or_string(val), units
+   return _nums_or_string(var)
 
 
 def _nums_or_string(var):
@@ -55,10 +68,16 @@ def _nums_or_string(var):
         return var
 
 
-class MyParser(argparse.ArgumentParser):
+class ErrorParser(argparse.ArgumentParser):
     """Used to control error placement a little better."""
+
+    def print_help(self, file=None):
+        test = StringIO()
+        self._print_message(self.format_help(), test)
+        print(test.getvalue().replace('@', '-'))
+
     def error(self, message):
-        print('error: %s\n' % message)
+        print(('error: %s\n' % message).replace('@', '-'))
         self.print_help()
         sys.exit(2)
 
@@ -82,59 +101,59 @@ class UnitHandler(argparse.Action):
         setattr(args, self.dest, values)
 
 
-# TODO: FIX NEGATIVE NUMBERS WITH UNITS.
 def _get_args(name):
     """Reads command line arguments and handles logic behind them."""
     arg_names = ['lat_ts', 'lat_0', 'long_0', 'delta_time']
     kwarg_names = ['center', 'pixel_size', 'units', 'shape', 'upper_left_extent', 'radius', 'area_extent',
                    'displacement_data', 'j', 'i', 'no_save', 'projection', 'projection_spheroid', 'earth_spheroid']
-    parser = MyParser(description='', allow_abbrev=False)
-    parser.add_argument('lat_ts', type=float, help='')
-    parser.add_argument('lat_0', type=float, help='')
-    parser.add_argument('long_0', type=float, help='')
+    my_parser = ErrorParser(description='', allow_abbrev=False, prefix_chars='@')
+
+    my_parser.add_argument('lat_ts', type=float, help='')
+    my_parser.add_argument('lat_0', type=float, help='')
+    my_parser.add_argument('long_0', type=float, help='')
     if name not in ['area', 'lat_long']:
-        parser.add_argument('delta_time', type=float, help='')
+        my_parser.add_argument('delta_time', type=float, help='')
     else:
         arg_names.remove('delta_time')
-    parser.add_argument('--center', nargs=2, type=_num_and_units, action=UnitHandler, help='center of projection: '
-                                                                                          'lat<units> '
-                                                                          'long<units>')
-    parser.add_argument('--pixel_size', nargs=2, type=_num_and_units, action=UnitHandler, help='size of pixels: '
+    my_parser.add_argument('@@center', nargs=2, type=_num_and_units, action=UnitHandler,
+                        help='center of projection: lat<units> long<units>')
+    my_parser.add_argument('@@pixel_size', nargs=2, type=_num_and_units, action=UnitHandler, help='size of pixels: '
                                                                                                'dy<units> dx<units>')
-    parser.add_argument('--units', default='m',
+    my_parser.add_argument('@@units', default='m',
                         help='units that provided arguments that take units (except center) should be interpreted as')
-    parser.add_argument('--shape', type=_nums_or_string, nargs=2,
+    my_parser.add_argument('@@shape', type=_nums_or_string, nargs=2,
                         help='number of pixels in the y and x direction: height width')
-    parser.add_argument('--upper_left_extent', nargs=2, type=_num_and_units, action=UnitHandler,
+    my_parser.add_argument('@@upper_left_extent', nargs=2, type=_num_and_units, action=UnitHandler,
                         help='projection y and x coordinates of the upper left'
                              'corner of the upper left pixel: y<units> x<units>')
-    parser.add_argument('--radius', nargs=2, type=_num_and_units, action=UnitHandler,
+    my_parser.add_argument('@@radius', nargs=2, type=_num_and_units, action=UnitHandler,
                         help='projection length from the center to the left/right'
                              'and top/bottom outer edges: dy<units> dx<units>')
-    parser.add_argument('--area_extent', nargs=4, type=_num_and_units, action=UnitHandler,
+    my_parser.add_argument('@@area_extent', nargs=4, type=_num_and_units, action=UnitHandler,
                         help='area extent as a list: y_ll<units> x_ll<units> y_ur<units> x_ur<units>')
-    parser.add_argument('--displacement_data', type=_nums_or_string, help='filename or list containing displacements')
+    my_parser.add_argument('@@displacement_data', type=_nums_or_string, help='filename or list containing displacements')
     if name != 'area':
-        parser.add_argument('--j', type=int, help='row to run calculations on')
-        parser.add_argument('--i', type=int, help='column to run calculations on')
+        my_parser.add_argument('@@j', type=int, help='row to run calculations on')
+        my_parser.add_argument('@@i', type=int, help='column to run calculations on')
     else:
         kwarg_names.remove('j')
         kwarg_names.remove('i')
     if name == 'wind_info':
-        parser.add_argument('--no_save', action="store_true", help="print data to shell without saving")
+        my_parser.add_argument('@@no_save', action="store_true", help="print data to shell without saving")
     else:
         kwarg_names.remove('no_save')
-    parser.add_argument('--projection', default='stere', help='name of projection that the image is in')
-    parser.add_argument('--projection_spheroid', default='WGS84', help='spheroid of projection')
+    my_parser.add_argument('@@projection', default='stere', help='name of projection that the image is in')
+    my_parser.add_argument('@@projection_spheroid', default='WGS84', help='spheroid of projection')
     if name not in ['area', 'lat_long']:
-        parser.add_argument('--earth_spheroid', default='WGS84', help='spheroid of Earth')
+        my_parser.add_argument('@@earth_spheroid', default='WGS84', help='spheroid of Earth')
     else:
         kwarg_names.remove('earth_spheroid')
-
-    commands = parser.parse_args()
+    argv = [arg.replace('--', '@@') for arg in sys.argv[1:]]
+    argv = [arg.replace('-h', '@h') for arg in argv]
+    commands = my_parser.parse_args(argv)
     args = [getattr(commands, arg) for arg in arg_names]
     kwargs = {kwarg: getattr(commands, kwarg) for kwarg in kwarg_names}
-    return args, kwargs, parser
+    return args, kwargs, my_parser
 
 
 def run_script(func, output_format, name, is_area=False, is_lat_long=False):
@@ -165,4 +184,4 @@ def run_script(func, output_format, name, is_area=False, is_lat_long=False):
         print(output_format(output, kwargs))
     except Exception:
         print(traceback.format_exc())
-        parser.parse_args(['-h'])
+        parser.parse_args(['@h'])
