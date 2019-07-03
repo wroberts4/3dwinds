8e3c2e9cf7659e063ef7ac9b28fb50222d8db087
wroberts <wroberts4@wisc.edu>
2019-07-03 16:58:30 -0500

    Use source but don't deactivate

--- make_env/run_scripts/area.sh
+++ make_env/run_scripts/area.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 PARENTDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. ~/anaconda3/etc/profile.d/conda.sh
-conda $PARENTDIR/env/bin/deactivate
-conda $PARENTDIR/env/bin/activate
+source $PARENTDIR/env/bin/activate
 python -W ignore -m pywinds.area "$@"
