597d3276cfdc0bcd4c6aaf5b8c87bb4f83713ff7
wroberts <wroberts4@wisc.edu>
2019-07-05 16:47:04 -0500

    Remove source deactivate

--- make_env/run_scripts/euclidean.sh
+++ make_env/run_scripts/euclidean.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 PARENTDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $PARENTDIR/env/bin/deactivate
-source $PARENTDIR/env/bin/activate
+source $PARENTDIR/env/bin/activate 2> /dev/null
 python -W ignore -m pywinds.euclidean "$@"
