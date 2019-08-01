f6df1d5a3b98558b4400cbeab0ddb203fff33c16
wroberts <wroberts4@wisc.edu>
2019-08-01 12:37:30 -0500

    Rename tarball to pywinds.tar.gz and improve conda-pack

--- make_env/create_package_env.sh
+++ make_env/create_package_env.sh
@@ -4,7 +4,7 @@ set -e
 PARENTDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Remove names that are needed and may have been made in the past.
-rm $PARENTDIR/new_pywinds.tar.gz 2> /dev/null ||
+rm $PARENTDIR/pywinds.tar.gz 2> /dev/null ||
 rm -r $PARENTDIR/pywinds 2> /dev/null ||
 
 . ~/anaconda3/etc/profile.d/conda.sh
@@ -12,16 +12,17 @@ conda env update -n pywinds -f $PARENTDIR/../build_environment.yml
 conda activate pywinds
 pip install -U --no-deps $PARENTDIR/..
 conda clean -afy
-conda-pack -o $PARENTDIR/new_pywinds.tar.gz --exclude sphinx
 mkdir $PARENTDIR/pywinds
 mkdir $PARENTDIR/pywinds/env
-tar -xzf $PARENTDIR/new_pywinds.tar.gz -C $PARENTDIR/pywinds/env
+conda-pack -j -1 --compress-level 9 -d $PARENTDIR/pywinds/env --exclude *.pyc --exclude *.pyx --exclude *.a
+tar -xzf $PARENTDIR/pywinds.tar.gz -C $PARENTDIR/pywinds/env
 mv $PARENTDIR/pywinds/env/bin/*.sh $PARENTDIR/pywinds
 mv $PARENTDIR/pywinds/env/bin/*.txt $PARENTDIR/pywinds
-rm $PARENTDIR/new_pywinds.tar.gz
+rm $PARENTDIR/pywinds.tar.gz
 
-find $PARENTDIR/pywinds -follow -type f -name '*.a' -exec rm -f {} \;
-find $PARENTDIR/pywinds -follow -type f -name '*.pyc' -exec rm -f {} \;
+find $PARENTDIR/pywinds -follow -type f -name 'pip' -exec rm -f {} \;
+find $PARENTDIR/pywinds -follow -type f -name 'wheel' -exec rm -f {} \;
+find $PARENTDIR/pywinds -follow -type f -name 'conda-pack' -exec rm -f {} \;
 
-tar -czf $PARENTDIR/new_pywinds.tar.gz pywinds
+tar -czf $PARENTDIR/pywinds.tar.gz pywinds
 rm -r $PARENTDIR/pywinds
