7d8237bd3093dc5bb54a57a32f2f5589b8d3f011
wroberts <wroberts4@wisc.edu>
2019-08-01 14:19:55 -0500

    Add .pyc files back in for faster execution

--- make_env/create_package_env.sh
+++ make_env/create_package_env.sh
@@ -9,20 +9,17 @@ rm -r $PARENTDIR/pywinds 2> /dev/null ||
 
 . ~/anaconda3/etc/profile.d/conda.sh
 conda env update -n pywinds -f $PARENTDIR/../build_environment.yml
+# BUG: conda activate not always found.
 conda activate pywinds
 pip install -U --no-deps $PARENTDIR/..
 conda clean -afy
 mkdir $PARENTDIR/pywinds
 mkdir $PARENTDIR/pywinds/env
-conda-pack -j -1 --compress-level 9 -d $PARENTDIR/pywinds/env --exclude *.pyc --exclude *.pyx --exclude *.a
+conda-pack -j -1 --compress-level 0 -d $PARENTDIR/pywinds/env --exclude *.a
 tar -xzf $PARENTDIR/pywinds.tar.gz -C $PARENTDIR/pywinds/env
 mv $PARENTDIR/pywinds/env/bin/*.sh $PARENTDIR/pywinds
 mv $PARENTDIR/pywinds/env/bin/*.txt $PARENTDIR/pywinds
 rm $PARENTDIR/pywinds.tar.gz
-
-find $PARENTDIR/pywinds -follow -type f -name 'pip' -exec rm -f {} \;
-find $PARENTDIR/pywinds -follow -type f -name 'wheel' -exec rm -f {} \;
-find $PARENTDIR/pywinds -follow -type f -name 'conda-pack' -exec rm -f {} \;
 
 tar -czf $PARENTDIR/pywinds.tar.gz pywinds
 rm -r $PARENTDIR/pywinds
